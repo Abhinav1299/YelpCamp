9ad5f1f719b3f63d6d7e63c089811c44c4477e8d
Abhinav Gupta <abhinavg1211@gmail.com>
2019-06-13 19:00:02 +0530

    added required packages and added main and content files

---
+++ app.js
@@ -0,0 +1,47 @@
+
+var express=require("express");
+var app=express();
+var bodyparser=require("body-parser");
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+
+var campgrounds = [
+    {name : "mt. everest" ,image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name : "mt. fuji" ,image:"https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name : "granite hill" ,image:"https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name : "mt. everest" ,image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name : "mt. fuji" ,image:"https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name : "granite hill" ,image:"https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name : "mt. everest" ,image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name : "mt. fuji" ,image:"https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name : "granite hill" ,image:"https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name : "granite hill" ,image:"https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}
+];
+
+
+app.get("/",function(req,res){
+    res.render("landing");
+});
+
+app.get("/campgrounds",function(req,res){
+    res.render("campgrounds",{campgrounds:campgrounds});
+});
+
+app.get("/campgrounds/new",function(req,res){
+    res.render("new");
+});
+
+app.post("/campgrounds",function(req,res){           //to add new capground
+    var name=req.body.name;
+    var image=req.body.image;
+    var newcampground={name:name,image:image};
+    campgrounds.push(newcampground);
+    res.redirect("/campgrounds")                    //default redirect is get request
+
+});                                    
+
+app.listen(3000,function(){
+    console.log("YelpCamp Server has started...");
+});
