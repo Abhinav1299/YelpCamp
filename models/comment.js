99dd8e80e97aae63296b2f69e0baa5b25d7d380b
Abhinav Gupta <abhinavg1211@gmail.com>
2019-06-14 18:18:36 +0530

    added campground and comment models

---
+++ models/comment.js
@@ -0,0 +1,9 @@
+
+
+var mongoose=require("mongoose");
+var commentschema= new mongoose.Schema({
+    text:String,
+    author:String
+});
+
+module.exports=mongoose.model("Comment",commentschema);  // name of the model =Comment
