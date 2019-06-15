72160801635d7bd023ff7dae9407255d705a139a
Abhinav Gupta <abhinavg1211@gmail.com>
2019-06-16 01:34:52 +0530

    updated comment schema

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,13 @@
 var mongoose=require("mongoose");
 var commentschema= new mongoose.Schema({
     text:String,
-    author:String
+    author:{
+        id:{
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User"
+        },
+        username:String
+    }
 });
 
 module.exports=mongoose.model("Comment",commentschema);  // name of the model =Comment
