15a474a1a7fa0aed2eab510c2fbdd92aaa62b00e
Abhinav Gupta <abhinavg1211@gmail.com>
2019-06-15 16:59:36 +0530

    updated seeds.js

--- seeds.js
+++ seeds.js
@@ -7,17 +7,17 @@ var data =[
     {
         name:"cloud's rest",
         image:"https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        description:"blah blah blah"
+        description:"Severe Tropical Cyclone Raja was a tropical cyclone that holds the 24-hour rainfall record of 674.9 mm (26.57 in) for the French Overseas Territory of Wallis and Futuna. The system was first noted by the Fiji Meteorological Service (FMS) as a weak tropical disturbance northeast of Tokelau in mid-December 1986. The system developed further as it moved southwest over the next few days, and it was classified as Tropical Cyclone Raja on 23 December. The newly renovated named system slowed and unexpectedly recurved southeast towards the French territory of Wallis and Futuna on 24 December. Over the next two days, Raja interacted with what would become Severe Tropical Cyclone Sally and executed a tight loop, passing within 55 km (35 mi) of Futuna. The system peaked as a Category 3 severe tropical cyclone on 28 December, with estimated 10-minute sustained winds of 150 km/h (90 mph). "
     },
     {
         name:"desert mesa",
         image:"https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        description:"blah blah blah"
+        description:"Severe Tropical Cyclone Raja was a tropical cyclone that holds the 24-hour rainfall record of 674.9 mm (26.57 in) for the French Overseas Territory of Wallis and Futuna. The system was first noted by the Fiji Meteorological Service (FMS) as a weak tropical disturbance northeast of Tokelau in mid-December 1986. The system developed further as it moved southwest over the next few days, and it was classified as Tropical Cyclone Raja on 23 December. The newly renovated named system slowed and unexpectedly recurved southeast towards the French territory of Wallis and Futuna on 24 December. Over the next two days, Raja interacted with what would become Severe Tropical Cyclone Sally and executed a tight loop, passing within 55 km (35 mi) of Futuna. The system peaked as a Category 3 severe tropical cyclone on 28 December, with estimated 10-minute sustained winds of 150 km/h (90 mph). "
     },
     {
         name:"canyon floor",
         image:"https://images.unsplash.com/photo-1545153996-e01b50d6ec38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",
-        description:"blah blah blah"
+        description:"Severe Tropical Cyclone Raja was a tropical cyclone that holds the 24-hour rainfall record of 674.9 mm (26.57 in) for the French Overseas Territory of Wallis and Futuna. The system was first noted by the Fiji Meteorological Service (FMS) as a weak tropical disturbance northeast of Tokelau in mid-December 1986. The system developed further as it moved southwest over the next few days, and it was classified as Tropical Cyclone Raja on 23 December. The newly renovated named system slowed and unexpectedly recurved southeast towards the French territory of Wallis and Futuna on 24 December. Over the next two days, Raja interacted with what would become Severe Tropical Cyclone Sally and executed a tight loop, passing within 55 km (35 mi) of Futuna. The system peaked as a Category 3 severe tropical cyclone on 28 December, with estimated 10-minute sustained winds of 150 km/h (90 mph). "
     }
 
 ]
